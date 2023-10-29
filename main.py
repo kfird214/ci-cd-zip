b7cbd6f15b03884daa322ecd00603d72ec3a51bc
kfir <kfir214@gmail.com>
2023-10-29 12:54:22 +0200

    zip folder

--- main.py
+++ main.py
@@ -1,28 +1,44 @@
-import zipfile, os, pathlib
+import zipfile, os, pathlib, argparse
+
+parser = argparse.ArgumentParser(description='Zip a folder')
+parser.add_argument('folder', type=str, help='The folder to zip')
+parser.add_argument('zip', type=str, help='The zip file to create')
+parser.add_argument('--ignore', type=str, nargs='+', help='The paths to ignore')
+args = parser.parse_args()
+
 
 # Define the folder to zip
-folder_to_zip = "input"
+folder_to_zip = args.folder
 rootPath = pathlib.Path(folder_to_zip)
 rootResolvedFolder = rootPath.resolve()
 
+print(f'Folder to zip: "{rootResolvedFolder}"')
+
 # Define the name of the zip file to create
-zip_file_rel_path = "out/out.zip"
+zip_file_rel_path = args.zip
 zip_file_path = pathlib.Path(zip_file_rel_path)
 zip_file_path.parent.mkdir(parents=True, exist_ok=True)
 
+print(f'Zip file: "{zip_file_path.resolve()}"')
 
 # Define the paths to ignore
-ignore_paths_relative = [
-    "ignore_dir",
-    "ignore.txt",
-    "dir/ignore.txt",
-]
+ignore_paths_relative = args.ignore
 
-ignore_paths = [os.path.realpath(os.path.join(rootResolvedFolder, p)) for p in ignore_paths_relative]
+ignore_paths = []
+
+if ignore_paths_relative is not None:
+    ignore_paths = [os.path.realpath(os.path.join(rootResolvedFolder, p)) for p in ignore_paths_relative]
+
+print()
+print(f'Ignore paths')
+for p in ignore_paths:
+    print(f'├─ "{p}"')
+print()
 
 # Create a list of files to add to the zip file
-
 files_to_zip = rootPath.rglob('*')
+
+# print(f"zipping {len(files_to_zip)} files")
 
 # Create the zip file
 with zipfile.ZipFile(zip_file_path, "w") as zip_file:
